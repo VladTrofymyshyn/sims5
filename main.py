0e69af73121060faa5badcf21ab8e478e16ae067
VladTrofymyshyn <vladtrofymyshyn@gmail.com>
2023-12-05 16:53:53 +0200

    додав атрибути

---
+++ main.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self,
+                 name = "Human",
+                 job = None,
+                 home = None,
+                 car = None):
+        self.name = name
+        self.money = 50_000
+        self.gladness = 50 #радість
+        self.satiety = 50 #харчування
+        self.job = job
+        self.car = car
+        self.home = home
