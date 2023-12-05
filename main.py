fd61fc1b2e498aa8d692f51b10e880464bbdae34
VladTrofymyshyn <vladtrofymyshyn@gmail.com>
2023-12-05 17:02:34 +0200

    дії які він робить

--- main.py
+++ main.py
@@ -6,8 +6,37 @@ class Human:
                  car = None):
         self.name = name
         self.money = 50_000
-        self.gladness = 50 #радість
-        self.satiety = 50 #харчування
+        self.gladness = 50
+        self.satiety = 50
         self.job = job
         self.car = car
         self.home = home
+        self.satiety = 50
+        self.job = job
+        self.car = car
+        self.home = home
+        self.home = home
+
+    def get_home(self):
+        pass
+
+    def get_car(self):
+        pass
+
+    def get_job(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def work(self):
+        pass
+
+    def shoping(self):
+        pass
+
+    def chill(self):
+        pass
+
+    def clean_home(self):
+        pass
